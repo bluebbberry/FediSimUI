9ec8b8dcb71a3b97cfcd13286633d6714f1de1aa
blueberry <jnb@campus.uni-paderborn.de>
2024-11-24 08:12:05 +0100

    feat: style website

--- fediverse-sim-ui/src/app/app.module.ts
+++ fediverse-sim-ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SimulationComponent } from './src/components/simulation/simulation.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
